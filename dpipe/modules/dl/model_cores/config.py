102df136d05bbae764d3f3e4e09549cd25cdd468
amigoml <amir14111@yandex.ru>
2017-08-25 13:42:52 +0300

    Update config.py

--- dpipe/modules/dl/model_cores/config.py
+++ dpipe/modules/dl/model_cores/config.py
@@ -1,12 +1,10 @@
 from dpipe.modules.dl.model_cores.unet import UNet2D, UResNet2D
 from .enet import ENet2D
 from .deepmedic_orig import DeepMedicOrig
-from .deepmedic_boris import DeepMedicFeature
 
 model_core_name2model_core = {
     'deepmedic_orig': DeepMedicOrig,
     'enet2d': ENet2D,
     'unet2d': UNet2D,
     'uresnet2d': UResNet2D,
-    'deepmedic_feature': DeepMedicFeature, 
 }
